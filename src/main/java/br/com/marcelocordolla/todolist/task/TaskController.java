dfa863003354ab338e97cfb7a441e84260efabb1
Marcelo <cordolla10@gmail.com>
2025-04-30 21:24:44 -0300

    add functions to create tasks commit

---
+++ src/main/java/br/com/marcelocordolla/todolist/task/TaskController.java
@@ -0,0 +1,28 @@
+package br.com.marcelocordolla.todolist.task;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+
+    @Autowired
+    private ITaskRepository taskRepository;
+
+    @PostMapping("/")
+    public ResponseEntity create(@RequestBody TaskModel taskModel){
+        var task = this.taskRepository.findByTitle(taskModel.getTitle());
+
+        if (task != null){
+            return ResponseEntity.status(400).body("Task ja existe");
+        }
+
+        var taskCriated = this.taskRepository.save(taskModel);
+        return ResponseEntity.status(200).body("Task cadastrado com sucesso");
+    }
+}
